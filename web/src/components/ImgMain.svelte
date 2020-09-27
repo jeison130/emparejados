2de14293100b44a82125b6edba05c33ee97345a3
jeison130 <jeison130@gmail.com>
2020-09-27 10:36:22 -0500

    Iniciando

---
+++ web/src/components/ImgMain.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let url
+</script>
+
+<div class="ratio-16-9 img-container">
+  <img
+    class="rounded-md"
+    src="{url}"
+    alt="" />
+</div>
---
+++ web/src/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<nav
+  class="flex items-center justify-between flex-wrap bg-teal-500 py-3 px-6 mt-3
+    mx-3 rounded-lg">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <span class="font-semibold text-xl tracking-tight">Juego de Parejas</span>
+  </div>
+</nav>
